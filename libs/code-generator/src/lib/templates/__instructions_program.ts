ab359d6ea5db7e72d186c45e904572e94b29cf97
Andres Guerrero <andresmgsl@gmail.com>
2021-10-10 11:30:03 +0200

    fix(code-generator): arguments kind and modifier added to context code.

--- libs/code-generator/src/lib/templates/__instructions_program.ts
+++ libs/code-generator/src/lib/templates/__instructions_program.ts
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 use crate::collections::Nombre_Coleccion;
 
 #[derive(Accounts)]
-#[instruction({{#each instruction.arguments}}{{#if @first}}{{else}}, {{/if}}{{this.data.name.camelCase}}: {{this.data.kind.name}}{{/each}})]
+#[instruction({{#each instruction.arguments}}, {{this.data.name.camelCase}}:{{#switch this.data.modifier.id}}{{#case '0'}}{{this.data.kind.name}}{{/case}}{{#case '1'}}[{{this.data.kind.name}};{{this.data.modifier.size}}]{{/case}}{{#case '2'}}Vec<{{this.data.kind.name}}>{{/case}}{{/switch}}{{/each}})]
 pub struct {{instruction.name.pascalCase}}<'info>{
     #[account(
 
