603cafe31e825b30faceaba199e1b1573fa40031
Andres Guerrero <andresmgsl@gmail.com>
2021-10-12 18:22:56 +0200

    feat(code-generator): handler instruction code added

--- libs/bulldozer/application/utils/services/code-generator/src/lib/templates/__instructions_context_program.ts
+++ libs/bulldozer/application/utils/services/code-generator/src/lib/templates/__instructions_context_program.ts
@@ -52,4 +52,11 @@ pub struct {{instruction.name.pascalCase}}<'info>{
     {{/if}}
     pub {{this.data.name.snakeCase}}: Program<'info, System>,
     {{/case}}{{/switch}}{{/each}}
-}`;
+}
+
+{{#if instruction.handler}}
+pub fn handler(ctx: Context<{{instruction.name.pascalCase}}>{{#each instruction.arguments}}, {{this.data.name.camelCase}}:{{#switch this.data.modifier.id}}{{#case '0'}}{{this.data.kind.name}}{{/case}}{{#case '1'}}[{{this.data.kind.name}};{{this.data.modifier.size}}]{{/case}}{{#case '2'}}Vec<{{this.data.kind.name}}>{{/case}}{{/switch}}{{/each}}) -> ProgramResult {
+{{{instruction.handler}}}
+}
+{{/if}}
+`;
