72eeb1d32b577e3faf7f6e8cc2ea6135c5712e83
Daniel Marin <danielarturomt@gmail.com>
2021-10-12 14:45:16 +0100

    refactor(app): move tabs to shell and clean store

--- libs/bulldozer/application/data-access/src/index.ts
+++ libs/bulldozer/application/data-access/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/application.store';
-export * from './lib/tabs.store';
 export * from './lib/collection.store';
 export * from './lib/instruction.store';
--- libs/bulldozer/application/shell/src/lib/shell.component.ts
+++ libs/bulldozer/application/shell/src/lib/shell.component.ts
@@ -1,12 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { WalletStore } from '@danmt/wallet-adapter-angular';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   ApplicationStore,
   CollectionStore,
   InstructionStore,
 } from '@heavy-duty/bulldozer/application/data-access';
-import { isNotNullOrUndefined } from '@heavy-duty/shared/utils/operators';
-import { merge } from 'rxjs';
 
 import { ApplicationShellStore } from './shell.store';
 
@@ -14,6 +11,25 @@ import { ApplicationShellStore } from './shell.store';
   selector: 'bd-application-shell',
   template: `
     <bd-navigation (downloadCode)="onDownloadCode()">
+      <nav mat-tab-nav-bar *ngIf="applicationId$ | ngrxPush as applicationId">
+        <div
+          mat-tab-link
+          class="flex items-center justify-between p-0"
+          *ngFor="let tab of tabs$ | ngrxPush"
+          [active]="(selectedTab$ | ngrxPush) === tab.id"
+        >
+          <a [routerLink]="['/applications', applicationId, tab.kind, tab.id]">
+            {{ tab.title }}
+          </a>
+          <button
+            mat-icon-button
+            (click)="onCloseTab($event, tab.id)"
+            [attr.aria-label]="'Close ' + tab.title + ' tab'"
+          >
+            <mat-icon>close</mat-icon>
+          </button>
+        </div>
+      </nav>
       <router-outlet></router-outlet>
     </bd-navigation>
   `,
@@ -26,27 +42,23 @@ import { ApplicationShellStore } from './shell.store';
     ApplicationShellStore,
   ],
 })
-export class ApplicationShellComponent implements OnInit {
+export class ApplicationShellComponent {
+  readonly applicationId$ = this._applicationStore.applicationId$;
+  readonly tabs$ = this._applicationShellStore.tabs$;
+  readonly selectedTab$ = this._applicationShellStore.selected$;
+
   constructor(
     private readonly _applicationShellStore: ApplicationShellStore,
-    private readonly _applicationStore: ApplicationStore,
-    private readonly _collectionStore: CollectionStore,
-    private readonly _instructionStore: InstructionStore,
-    private readonly _walletStore: WalletStore
+    private readonly _applicationStore: ApplicationStore
   ) {}
-
-  ngOnInit() {
-    this._applicationShellStore.notifyErrors(
-      merge(
-        this._applicationStore.error$,
-        this._collectionStore.error$,
-        this._instructionStore.error$,
-        this._walletStore.error$
-      ).pipe(isNotNullOrUndefined)
-    );
-  }
 
   onDownloadCode() {
     this._applicationShellStore.downloadCode();
+  }
+
+  onCloseTab(event: Event, tabId: string) {
+    event.stopPropagation();
+    event.preventDefault();
+    this._applicationShellStore.closeTab(tabId);
   }
 }
