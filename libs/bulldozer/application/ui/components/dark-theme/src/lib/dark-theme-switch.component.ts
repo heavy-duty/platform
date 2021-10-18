f12fa76de26942a47dee1108da5a6a02bbda08bf
Andres Guerrero <andresmgsl@gmail.com>
2021-10-19 01:30:50 +0200

    feat(dark-theme-switch): new dark-theme switch component added

---
+++ libs/bulldozer/application/ui/components/dark-theme/src/lib/dark-theme-switch.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { DarkThemeService } from '@heavy-duty/bulldozer/application/utils/services/dark-theme';
+import { map } from 'rxjs/operators';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'bd-dark-theme-switch',
+  template: `
+    <button
+      color="accent"
+      mat-mini-fab
+      aria-label="Dark theme switch"
+      [bdDarkTheme]="isDarkThemeEnabled$ | async"
+      (click)="toggleDarkMode()"
+      [ngStyle]="buttonDynamicStyle$ | async"
+    >
+      <mat-icon>{{ currentIconSelected$ | async }}</mat-icon>
+    </button>
+  `,
+  styles: [],
+})
+export class DarkThemeSwitchComponent {
+  readonly isDarkThemeEnabled$ = this._themeService.isDarkThemeEnabled$;
+  readonly currentIconSelected$ = this.isDarkThemeEnabled$.pipe(
+    map((isDarkThemeEnabled) =>
+      isDarkThemeEnabled ? 'light_mode' : 'dark_mode'
+    )
+  );
+  readonly buttonDynamicStyle$ = this.isDarkThemeEnabled$.pipe(
+    map((isDarkThemeEnabled) =>
+      isDarkThemeEnabled ? { color: 'white' } : { color: 'black' }
+    )
+  );
+
+  constructor(private readonly _themeService: DarkThemeService) {}
+
+  toggleDarkMode() {
+    this._themeService.toggleDarkTheme();
+  }
+}
