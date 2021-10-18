bcfe8515bd1b9941ceb194fe87b3c6516daa2a00
Daniel Marin <danielarturomt@gmail.com>
2021-10-18 20:38:43 +0100

    refactor(bulldozer:client): use checkbox for modifier in edit signer (#54)
    * refactor(bulldozer:client): use checkbox for modifier in edit signer
    
    * fix(bulldozer:client): use the right default and validators
    
    * chore(bulldozer:client): clean up imports

--- libs/bulldozer/application/features/edit-signer/src/lib/edit-signer.module.ts
+++ libs/bulldozer/application/features/edit-signer/src/lib/edit-signer.module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { ObscureAddressModule } from '@heavy-duty/bulldozer/application/utils/pipes/obscure-address';
 
 import { EditSignerComponent } from './edit-signer.component';
 
@@ -16,12 +15,11 @@ import { EditSignerComponent } from './edit-signer.component';
     CommonModule,
     ReactiveFormsModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatSelectModule,
-    ObscureAddressModule,
   ],
   declarations: [EditSignerComponent],
 })
