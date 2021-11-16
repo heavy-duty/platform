fd722b3e15b0c41086ad2f83100f9b6e2ee13f6b
Daniel Marin <danielarturomt@gmail.com>
2021-11-16 15:25:23 +0000

    fix(bulldozer:client): display the argument kind accordingly

--- libs/bulldozer/application/utils/services/code-generator/src/lib/templates/__full_program.ts
+++ libs/bulldozer/application/utils/services/code-generator/src/lib/templates/__full_program.ts
@@ -12,7 +12,7 @@ pub mod {{application.name.snakeCase}} {
   use super::*;
 
   {{#each application.instructions}}
-  pub fn {{this.data.name.snakeCase}}(ctx: Context<{{this.data.name.pascalCase}}>{{#each this.arguments}}, {{this.data.name.camelCase}}:{{#switch this.data.modifier.id}}{{#case '0'}}{{this.data.kind.name}}{{/case}}{{#case '1'}}[{{this.data.kind.name}};{{this.data.modifier.size}}]{{/case}}{{#case '2'}}Vec<{{this.data.kind.name}}>{{/case}}{{/switch}}{{/each}}) -> ProgramResult {
+  pub fn {{this.data.name.snakeCase}}(ctx: Context<{{this.data.name.pascalCase}}>{{#each this.arguments}}, {{this.data.name.snakeCase}}: {{#switch this.data.modifier.id}}{{#case null}}{{this.data.kind.name}}{{/case}}{{#case '0'}}[{{this.data.kind.name}};{{this.data.modifier.size}}]{{/case}}{{#case '1'}}Vec<{{this.data.kind.name}}>{{/case}}{{/switch}}{{/each}}) -> ProgramResult {
     instructions::{{this.data.name.snakeCase}}::handler(ctx{{#each this.arguments}}, {{this.data.name.camelCase}}{{/each}})
   }
   {{/each}}
