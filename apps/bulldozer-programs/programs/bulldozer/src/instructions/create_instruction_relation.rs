067d000140abc75cf4b88c3f0886b9a822d6ae4c
Daniel Marin <danielarturomt@gmail.com>
2021-12-21 16:39:44 +0000

    feat(bulldozer-programs): add workspace to instruction relation

--- apps/bulldozer-programs/programs/bulldozer/src/instructions/create_instruction_relation.rs
+++ apps/bulldozer-programs/programs/bulldozer/src/instructions/create_instruction_relation.rs
@@ -1,4 +1,6 @@
-use crate::collections::{Application, Instruction, InstructionAccount, InstructionRelation};
+use crate::collections::{
+  Application, Instruction, InstructionAccount, InstructionRelation, Workspace,
+};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
@@ -7,7 +9,7 @@ pub struct CreateInstructionRelation<'info> {
   #[account(
         init,
         payer = authority,
-        space = 8 + 32 + 32 + 32 + 32 + 32 + 1,
+        space = 8 + 32 + 32 + 32 + 32 + 32 + 32 + 1,
         seeds = [
           b"instruction_relation",
           from.key().as_ref(),
@@ -17,6 +19,7 @@ pub struct CreateInstructionRelation<'info> {
         constraint = from.key().as_ref() != to.key().as_ref()
     )]
   pub relation: Box<Account<'info, InstructionRelation>>,
+  pub workspace: Box<Account<'info, Workspace>>,
   pub application: Box<Account<'info, Application>>,
   pub instruction: Box<Account<'info, Instruction>>,
   pub from: Box<Account<'info, InstructionAccount>>,
@@ -29,6 +32,7 @@ pub struct CreateInstructionRelation<'info> {
 pub fn handler(ctx: Context<CreateInstructionRelation>, bump: u8) -> ProgramResult {
   msg!("Create instruction relation");
   ctx.accounts.relation.authority = ctx.accounts.authority.key();
+  ctx.accounts.relation.workspace = ctx.accounts.workspace.key();
   ctx.accounts.relation.application = ctx.accounts.application.key();
   ctx.accounts.relation.instruction = ctx.accounts.instruction.key();
   ctx.accounts.relation.from = ctx.accounts.from.key();
