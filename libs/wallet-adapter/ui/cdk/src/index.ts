dc72c75ab29f52d4b557d2c589534d761fa52f82
Daniel Marin <danielarturomt@gmail.com>
2022-01-23 20:07:43 +0000

    fix(wallet-adapter): prevent building errors

--- libs/wallet-adapter/ui/cdk/src/index.ts
+++ libs/wallet-adapter/ui/cdk/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/connect-button.directive';
 export * from './lib/disconnect-button.directive';
+export * from './lib/list-item.component';
 export * from './lib/obscure-address.pipe';
 export * from './lib/wallet-adapter-cdk.module';
 export * from './lib/wallet-adapter.directive';
