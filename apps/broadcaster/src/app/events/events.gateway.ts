f9255cf8c94c60aee7b11064c95edb1230052bbd
Daniel Marin <danielarturomt@gmail.com>
2022-03-12 10:18:03 +0000

    refactor(broadcaster): clean up

--- apps/broadcaster/src/app/events/events.gateway.ts
+++ apps/broadcaster/src/app/events/events.gateway.ts
@@ -1,9 +1,10 @@
 import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  OnGatewayInit,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
@@ -14,17 +15,11 @@ import WebSocket, { Server } from 'ws';
     origin: '*',
   },
 })
-export class EventsGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private readonly _logger = new Logger(EventsGateway.name);
   @WebSocketServer()
   server: Server;
   private readonly _topics = new Map<string, Set<WebSocket>>();
-
-  afterInit() {
-    this._topics.set('*', new Set<WebSocket>());
-  }
 
   handleConnection(
     @ConnectedSocket()
@@ -45,9 +40,53 @@ export class EventsGateway
       `Client disconnected. [${this.server.clients.size} clients connected]`
     );
 
-    this._topics.set(
-      '*',
-      new Set([...(this._topics.get('*') ?? [])].filter((ws) => ws !== client))
+    this._topics.forEach((_, topic) => {
+      const clients = [...(this._topics.get(topic) ?? [])].filter(
+        (ws) => ws !== client
+      );
+
+      if (clients.length === 0) {
+        this._topics.delete(topic);
+      } else {
+        this._topics.set(topic, new Set(clients));
+      }
+    });
+  }
+
+  @SubscribeMessage('subscribe')
+  onSubscribe(
+    @ConnectedSocket()
+    client: WebSocket,
+    @MessageBody() topics: string[]
+  ) {
+    this._logger.log(`Client subscribed to [${topics.join(', ')}].`);
+
+    topics.forEach((topic) =>
+      this._topics.set(
+        topic,
+        new Set([...(this._topics.get(topic) ?? []), client])
+      )
     );
+  }
+
+  @SubscribeMessage('unsubscribe')
+  onUnsubscribe(
+    @ConnectedSocket()
+    client: WebSocket,
+    @MessageBody() topics: string[]
+  ) {
+    this._logger.log(`Client unsubscribed from [${topics.join(', ')}].`);
+
+    topics.forEach((topic) => {
+      const clients = [...(this._topics.get(topic) ?? [])].filter(
+        (ws) => ws !== client
+      );
+
+      if (clients.length === 0) {
+        this._topics.delete(topic);
+      } else {
+        this._topics.set(topic, new Set(clients));
+      }
+    });
   }
 }
