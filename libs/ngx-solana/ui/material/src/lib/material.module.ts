bcfb31841c03271270d81ef062429963edee7e1e
Daniel Marin <danielarturomt@gmail.com>
2022-03-14 19:41:05 +0000

    feat(bulldozer-client): display more info about my txs

--- libs/ngx-solana/ui/material/src/lib/material.module.ts
+++ libs/ngx-solana/ui/material/src/lib/material.module.ts
@@ -8,6 +8,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
@@ -38,6 +39,7 @@ import { HdTransactionsListTriggerDirective } from './transactions-list.trigger.
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     MatMenuModule,
     MatProgressSpinnerModule,
     MatSelectModule,
