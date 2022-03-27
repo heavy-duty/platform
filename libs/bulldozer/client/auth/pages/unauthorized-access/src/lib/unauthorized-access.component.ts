f09997e88464476a4a6405ec6d9635ffd3890c6d
Andres Guerrero <andresmgsl@gmail.com>
2022-03-27 12:10:09 +0100

    refactor(bulldozer-client): unauthorized Component design updated

--- libs/bulldozer/client/auth/pages/unauthorized-access/src/lib/unauthorized-access.component.ts
+++ libs/bulldozer/client/auth/pages/unauthorized-access/src/lib/unauthorized-access.component.ts
@@ -3,24 +3,64 @@ import {
   Component,
   HostBinding,
   OnDestroy,
-  OnInit
+  OnInit,
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { filter, Subject, takeUntil, withLatestFrom } from 'rxjs';
 
+// <header class="mt-16 flex justify-center flex-wrap">
+//    <figure class="mb-4 w-80">
+//      <img src="assets/images/logo.png" class="w-full" />
+//    </figure>
+//    <h1 class="w-full text-center">BULLDOZER</h1>
+//    <p class="w-full text-center">Connect your wallet to start building</p>
+// </header>
+// <main class="flex justify-center">
+//    <hd-wallet-multi-button color="primary"></hd-wallet-multi-button>
+// </main>
+
 @Component({
   selector: 'bd-unauthorized-access',
   template: `
-    <header class="mt-16 flex justify-center flex-wrap">
-      <figure class="mb-4 w-80">
-        <img src="assets/images/logo.png" class="w-full" />
-      </figure>
-      <h1 class="w-full text-center">BULLDOZER</h1>
-      <p class="w-full text-center">Connect your wallet to start building</p>
-    </header>
-    <main class="flex justify-center">
-      <hd-wallet-multi-button color="primary"></hd-wallet-multi-button>
+    <main class="flex h-screen">
+      <div class="hd-side-left w-1/2 h-screen flex justify-end items-center">
+        <div class="content mr-16 -scroll-mt-6">
+          <figure class="mb-4 w-32">
+            <img src="assets/images/logo.png" class="w-full" />
+          </figure>
+          <h2 class="mb-1 font-bold">BULLDOZER</h2>
+          <p class="mb-7">Connect your wallet to start building</p>
+          <hd-wallet-multi-button color="primary"></hd-wallet-multi-button>
+        </div>
+      </div>
+      <div class="hd-side-right w-1/2 h-screen flex justify-start items-center">
+        <div class="content ml-16 w-96 -scroll-mt-6">
+          <h2 class="font-bold">WELCOME</h2>
+          <p>
+            Bulldozer is a open source low code platform to build Solana
+            programs. It gives developers the ability to manage their program’s
+            ecosystem through a UI, hiding all the gory details.
+          </p>
+          <div class="flex mt-6">
+            <figure class="w-8 mr-4">
+              <a href="https://github.com/heavy-duty/platform" target="_blank">
+                <img src="assets/images/social/github.png" class="w-full" />
+              </a>
+            </figure>
+            <figure class="w-8 mr-4">
+              <a href="https://discord.gg/Ej47EUAj4u" target="_blank">
+                <img src="assets/images/social/discord.png" class="w-full" />
+              </a>
+            </figure>
+            <figure class="w-8 mr-4">
+              <a href="https://twitter.com/HeavyDutyBuild" target="_blank">
+                <img src="assets/images/social/twitter.png" class="w-full" />
+              </a>
+            </figure>
+          </div>
+        </div>
+      </div>
     </main>
   `,
   styles: [],
