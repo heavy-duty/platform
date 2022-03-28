66bfe0978d117f632d932e0f601262e21da2ab78
Andres Guerrero <andresmgsl@gmail.com>
2022-03-28 16:38:09 +0200

    refactor(bulldozer-client): broadcaster API port changed

--- apps/broadcaster/src/main.ts
+++ apps/broadcaster/src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 4040;
   await app.listen(port);
   Logger.log(`🚀 Broadcaster is running on: ws://localhost:${port}`);
 }
