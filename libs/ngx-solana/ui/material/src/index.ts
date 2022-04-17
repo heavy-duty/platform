b5b2caa0b0249d53adf98328bda230e323f8aae6
Andres Guerrero <andresmgsl@gmail.com>
2022-04-17 11:11:36 +0100

    fix(bulldozer-client): all necessary files to build app exported

--- libs/ngx-solana/ui/material/src/index.ts
+++ libs/ngx-solana/ui/material/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/connection-menu.component';
 export * from './lib/connection-status.component';
-export * from './lib/edit-endpoints-modal.component';
 export * from './lib/edit-endpoints-modal.directive';
 export * from './lib/endpoints-list.component';
 export * from './lib/material.module';
