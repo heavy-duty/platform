e928bb971d168a5615b1dc5165f32c01caada45e
Daniel Marin <danielarturomt@gmail.com>
2022-05-12 22:21:26 +0100

    chore(core): migrate nx latest

---
+++ libs/bulldozer/client/workspaces/modals/edit-workspace/jest.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+export default {
+	displayName: 'bulldozer-client-workspaces-modals-edit-workspace',
+	preset: '../../../../../../jest.preset.js',
+	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+			stringifyContentPathRegex: '\\.(html|svg)$',
+		},
+	},
+	coverageDirectory:
+		'../../../../../../coverage/libs/bulldozer/client/workspaces/modals/edit-workspace',
+	transform: {
+		'^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	snapshotSerializers: [
+		'jest-preset-angular/build/serializers/no-ng-attributes',
+		'jest-preset-angular/build/serializers/ng-snapshot',
+		'jest-preset-angular/build/serializers/html-comment',
+	],
+};
