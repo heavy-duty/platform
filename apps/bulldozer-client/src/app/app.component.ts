ff7e385fa9f28397022d66d02fbc505d5f0e5153
Daniel Marin <danielarturomt@gmail.com>
2022-05-13 17:33:25 +0100

    chore: testing nx affected

--- apps/bulldozer-client/src/app/app.component.ts
+++ apps/bulldozer-client/src/app/app.component.ts
@@ -2,28 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletWalletAdapter,
-  SolongWalletAdapter,
+	PhantomWalletAdapter,
+	SlopeWalletAdapter,
+	SolflareWalletAdapter,
+	SolletWalletAdapter,
+	SolongWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 
 @Component({
-  selector: 'bd-root',
-  template: ` <router-outlet></router-outlet> `,
-  styles: [],
+	selector: 'bd-root',
+	template: ` <router-outlet></router-outlet> `,
+	styles: [],
 })
 export class AppComponent implements OnInit {
-  constructor(private readonly _walletStore: WalletStore) {}
+	constructor(private readonly _walletStore: WalletStore) {}
 
-  ngOnInit() {
-    this._walletStore.setAdapters([
-      new PhantomWalletAdapter(),
-      new SolletWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-      new SlopeWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new SolongWalletAdapter(),
-    ]);
-  }
+	ngOnInit() {
+		console.log('testing nx affected');
+		this._walletStore.setAdapters([
+			new PhantomWalletAdapter(),
+			new SolletWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+			new SlopeWalletAdapter(),
+			new SolflareWalletAdapter(),
+			new SolongWalletAdapter(),
+		]);
+	}
 }
