70273d9ad3a311058d1fef9320db738a8e360e81
Andres Guerrero <andresmgsl@gmail.com>
2022-05-16 15:30:18 +0100

    feat: initial message when use CLI added

--- apps/bulldozer-cli/src/app/utils/log.ts
+++ apps/bulldozer-cli/src/app/utils/log.ts
@@ -1,7 +1,38 @@
 // import logSymbols from 'log-symbols';
+import { LoggerService } from '@nestjs/common';
+import * as figlet from 'figlet';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const chalk = require('chalk');
+const orange = chalk.hex('#FFA500');
+
+export class BulldozerLogger implements LoggerService {
+	log(message: any, ...optionalParams: any[]) {
+		console.log(chalk.blueBright(message));
+	}
+
+	error(message: any, ...optionalParams: any[]) {
+		console.log(chalk.redBright(message));
+	}
+
+	warn(message: any, ...optionalParams: any[]) {
+		console.log(chalk.yellowBright(message));
+	}
+
+	introMessage() {
+		console.log(
+			orange(
+				figlet.textSync('Bulldozer', {
+					font: 'Graffiti',
+					horizontalLayout: 'full',
+				})
+			)
+		);
+	}
+}
 
 export function log(message: string) {
-  console.log(message);
+	console.log(message);
 }
 
 // export function warn(message = '') {
--- apps/bulldozer-cli/src/main.ts
+++ apps/bulldozer-cli/src/main.ts
@@ -1,15 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { config } from 'dotenv';
 import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app/app.module';
+import { BulldozerLogger } from './app/utils';
 
 async function bootstrap() {
 	config();
-
+	const logger = new BulldozerLogger();
+	logger.introMessage();
 	await CommandFactory.run(AppModule, ['warn', 'error']);
 }
 
