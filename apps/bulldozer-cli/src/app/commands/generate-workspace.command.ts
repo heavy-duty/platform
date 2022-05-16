bba581db5c198fcead0819b70de58fbbbf4cf3a4
Andres Guerrero <andresmgsl@gmail.com>
2022-05-16 14:29:08 +0100

    fix: generate workspace command message fixed

--- apps/bulldozer-cli/src/app/commands/generate-workspace.command.ts
+++ apps/bulldozer-cli/src/app/commands/generate-workspace.command.ts
@@ -3,7 +3,7 @@ import { Command, CommandRunner } from 'nest-commander';
 @Command({
 	name: 'generate-workspace',
 	description:
-		'Generate the source code for all the Apps in a Workspace. You need to pass two arguments, one to specify the workspace id in which you have the app you want to generate the source code. The other argument is the app id, to select the app',
+		'Generate the source code for all the Apps in a Workspace. You need to pass only one argument to specify the workspace id in which you have the app you want to generate the source code.',
 	arguments: '<workspace-id>',
 	argsDescription: {
 		'workspace-id': 'The workspace id which you want to select',
