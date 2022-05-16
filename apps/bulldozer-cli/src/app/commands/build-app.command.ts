03b265fd60229b76b32881bd0d5954683542092f
Andres Guerrero <andresmgsl@gmail.com>
2022-05-16 14:28:53 +0100

    feat: build app command added

---
+++ apps/bulldozer-cli/src/app/commands/build-app.command.ts
@@ -0,0 +1,17 @@
+import { Command, CommandRunner } from 'nest-commander';
+
+@Command({
+	name: 'build-app',
+	description:
+		' Generate the source code for all the App in a Workspace and build them using Anchor Build command under the hood. You need to pass two arguments, one to specify the workspace id in which you have the app you want to generate the source code. The other argument is the app id, to select it',
+	arguments: '<workspace-id> <application-id>',
+	argsDescription: {
+		'workspace-id': 'The workspace id which you want to select',
+		'application-id': 'The App id which you want to select ',
+	},
+})
+export class GenerateAppCommand implements CommandRunner {
+	async run(params: string[]) {
+		console.log(`Workspace ID: ${params[0]} App Id: ${params[1]}`);
+	}
+}
