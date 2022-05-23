0c0614960977be31a4fc2d43bbc7213042d7b902
Andres Guerrero <andresmgsl@gmail.com>
2022-05-23 14:20:01 +0100

    chore: changed drill web-client URL

--- apps/drill-web-client/src/environments/environment.prod.ts
+++ apps/drill-web-client/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
 	programId: '2fg324Gf51Nrp1jzxXETvJmXPEiHz9ybNY1r575MtijW',
 	rpcEndpoint: 'https://api.devnet.solana.com',
 	githubRepository: 'andresmgsl/monorepo',
-	redirectUri: 'http://localhost:4200/login',
+	redirectUri: 'https://drill.heavyduty.builders/login',
 	githubOAuth: 'https://github.com/login/oauth/authorize',
 	clientId: 'Iv1.ec9039b697c3dda5',
-	gatewayUrl: 'https://drill-gateway.heavyduty.builders/',
+	gatewayUrl: 'https://drill-gateway.heavyduty.builders',
 };
