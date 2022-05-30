8679f4c5dd0c52e7f64d988b238c85bba7553a28
Andres Guerrero <andresmgsl@gmail.com>
2022-05-30 20:16:18 +0100

    chore: enviroment updated with better data

--- apps/drill-web-client/src/environments/environment.prod.ts
+++ apps/drill-web-client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
 	production: true,
 	githubApi: 'https://api.github.com',
-	programId: '2fg324Gf51Nrp1jzxXETvJmXPEiHz9ybNY1r575MtijW',
+	programId: 'DR1LL87tP9uZqPXTxGDVrVE53zrLTBei3YLKrx4ihYh1',
 	rpcEndpoint: 'https://api.devnet.solana.com',
-	githubRepository: 'andresmgsl/monorepo',
-	redirectUri: 'https://drill.heavyduty.builders/login',
+	githubRepository: '<username>/<repository>',
+	redirectUri: '<drill-frontend-app>',
 	githubOAuth: 'https://github.com/login/oauth/authorize',
 	clientId: 'Iv1.ec9039b697c3dda5',
-	gatewayUrl: 'https://drill-gateway.heavyduty.builders',
+	gatewayUrl: '<drill-gateway-app>',
 };
--- apps/drill-web-client/src/environments/environment.ts
+++ apps/drill-web-client/src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
 	production: false,
-	githubApi: 'https://api.github.com',
-	programId: '2fg324Gf51Nrp1jzxXETvJmXPEiHz9ybNY1r575MtijW',
-	rpcEndpoint: 'https://api.devnet.solana.com',
-	githubRepository: 'andresmgsl/monorepo',
-	redirectUri: 'https://drill.heavyduty.builders/login',
-	githubOAuth: 'https://github.com/login/oauth/authorize',
-	clientId: 'Iv1.ec9039b697c3dda5',
-	gatewayUrl: 'https://drill-gateway.heavyduty.builders',
+	githubApi: '',
+	programId: '',
+	rpcEndpoint: '',
+	githubRepository: '',
+	redirectUri: '',
+	githubOAuth: '',
+	clientId: '',
+	gatewayUrl: '',
 };
 
 /*
