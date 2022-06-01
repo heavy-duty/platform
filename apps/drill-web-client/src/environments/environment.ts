35170fd7c85948fcd39369d3e9382a17ba912fd3
Andres Guerrero <andresmgsl@gmail.com>
2022-06-01 17:23:43 +0100

    fix: added values to test in enviroment.ts file

--- apps/drill-web-client/src/environments/environment.ts
+++ apps/drill-web-client/src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
 	production: false,
-	githubApi: '',
-	programId: '',
-	rpcEndpoint: '',
-	githubRepository: '',
-	redirectUri: '',
-	githubOAuth: '',
-	clientId: '',
-	gatewayUrl: '',
+	githubApi: 'https://api.github.com',
+	programId: '2fg324Gf51Nrp1jzxXETvJmXPEiHz9ybNY1r575MtijW',
+	rpcEndpoint: 'https://api.devnet.solana.com',
+	githubRepository: 'danmt/kin-bounty-sample',
+	redirectUri: 'http://localhost:4200/login',
+	githubOAuth: 'https://github.com/login/oauth/authorize',
+	clientId: 'Iv1.9c36914b7e824485',
+	gatewayUrl: 'http://localhost:3333',
 };
 
 /*
