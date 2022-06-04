136d7a83d6666a233c6fb29b103cd969bc24a302
Daniel Marin <danielarturomt@gmail.com>
2022-06-04 17:19:05 +0100

    feat(blueprint): handle modal in triggering in cdk

--- libs/wallet-adapter/ui/cdk/src/lib/wallet-adapter-cdk.module.ts
+++ libs/wallet-adapter/ui/cdk/src/lib/wallet-adapter-cdk.module.ts
@@ -1,31 +1,35 @@
+import { DialogModule } from '@angular/cdk/dialog';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HdWalletConnectButtonDirective } from './connect-button.directive';
 import { HdWalletDisconnectButtonDirective } from './disconnect-button.directive';
 import { HdSanitizeUrlPipe } from './internals';
 import { HdWalletListItemComponent } from './list-item.component';
+import { HdWalletModalButtonDirective } from './modal-button.directive';
 import { HdObscureAddressPipe } from './obscure-address.pipe';
 import { HdWalletAdapterDirective } from './wallet-adapter.directive';
 import { HdWalletIconComponent } from './wallet-icon.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    HdWalletConnectButtonDirective,
-    HdWalletDisconnectButtonDirective,
-    HdSanitizeUrlPipe,
-    HdObscureAddressPipe,
-    HdWalletIconComponent,
-    HdWalletAdapterDirective,
-    HdWalletListItemComponent,
-  ],
-  exports: [
-    HdWalletConnectButtonDirective,
-    HdWalletDisconnectButtonDirective,
-    HdObscureAddressPipe,
-    HdWalletIconComponent,
-    HdWalletAdapterDirective,
-    HdWalletListItemComponent,
-  ],
+	imports: [CommonModule, DialogModule],
+	declarations: [
+		HdWalletConnectButtonDirective,
+		HdWalletDisconnectButtonDirective,
+		HdSanitizeUrlPipe,
+		HdObscureAddressPipe,
+		HdWalletIconComponent,
+		HdWalletAdapterDirective,
+		HdWalletListItemComponent,
+		HdWalletModalButtonDirective,
+	],
+	exports: [
+		HdWalletConnectButtonDirective,
+		HdWalletDisconnectButtonDirective,
+		HdObscureAddressPipe,
+		HdWalletIconComponent,
+		HdWalletAdapterDirective,
+		HdWalletListItemComponent,
+		HdWalletModalButtonDirective,
+	],
 })
 export class HdWalletAdapterCdkModule {}
