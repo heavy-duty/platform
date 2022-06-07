5ce016da35557b2710d830f217fae058b7e06343
Daniel Marin <danielarturomt@gmail.com>
2022-06-07 23:52:41 +0100

    chore(crane): run formatting

--- apps/crane-web-client/src/app/machines/sign-transaction.machine.typegen.ts
+++ apps/crane-web-client/src/app/machines/sign-transaction.machine.typegen.ts
@@ -1,31 +1,31 @@
 // This file was automatically generated. Edits will be overwritten
 
 export interface Typegen0 {
-  '@@xstate/typegen': true;
-  eventsCausingActions: {
-    'Save transaction in context': 'startSigning';
-    'Sign using keypair': 'signTransactionWithKeypair';
-    'Save signature in context': 'signTransactionWithWallet';
-    'Save transaction signed in context': '';
-  };
-  internalEvents: {
-    '': { type: '' };
-    'xstate.init': { type: 'xstate.init' };
-  };
-  invokeSrcNameMap: {};
-  missingImplementations: {
-    actions: never;
-    services: never;
-    guards: never;
-    delays: never;
-  };
-  eventsCausingServices: {};
-  eventsCausingGuards: {
-    'is fire and forget': '';
-    'valid signer': 'signTransactionWithKeypair' | 'signTransactionWithWallet';
-    'signatures done': '';
-  };
-  eventsCausingDelays: {};
-  matchesStates: 'Idle' | 'Transaction signed' | 'Sign transaction' | 'Done';
-  tags: never;
+	'@@xstate/typegen': true;
+	eventsCausingActions: {
+		'Save transaction in context': 'startSigning';
+		'Sign using keypair': 'signTransactionWithKeypair';
+		'Save signature in context': 'signTransactionWithWallet';
+		'Save transaction signed in context': '';
+	};
+	internalEvents: {
+		'': { type: '' };
+		'xstate.init': { type: 'xstate.init' };
+	};
+	invokeSrcNameMap: {};
+	missingImplementations: {
+		actions: never;
+		services: never;
+		guards: never;
+		delays: never;
+	};
+	eventsCausingServices: {};
+	eventsCausingGuards: {
+		'is fire and forget': '';
+		'valid signer': 'signTransactionWithKeypair' | 'signTransactionWithWallet';
+		'signatures done': '';
+	};
+	eventsCausingDelays: {};
+	matchesStates: 'Idle' | 'Transaction signed' | 'Sign transaction' | 'Done';
+	tags: never;
 }
