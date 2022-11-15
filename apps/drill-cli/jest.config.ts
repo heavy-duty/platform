76e910fb96f1700e9184bb92a7809e242c0c5e79
Dan M <danielarturomt@gmail.com>
2022-11-15 16:27:28 +0000

    chore(core): bump up all dependencies

---
+++ apps/drill-cli/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+module.exports = {
+	displayName: 'drill-cli',
+	preset: '../../jest.preset.js',
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+		},
+	},
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/apps/drill-cli',
+};
